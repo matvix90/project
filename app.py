360f18d59b78f88b983a03be7ce66b078adc876c
matvix90 <matvix90@gmail.com>
2023-09-24 16:24:08 +0200

    #

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, abort, session
+from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from cs50 import SQL
@@ -6,14 +7,39 @@ from cs50 import SQL
 from random import choices
 from string import digits
 
+from helpers import login_required, get_location
+
+import folium
+
+# Configure application
 app = Flask(__name__)
 
+# Configure secret Key
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure database
 db = SQL("sqlite:///data.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 
 @app.route("/")
 def start():
+
+    # Forget any user_id
+    session.clear()
 
     db.execute("DELETE FROM users")
     username = "admin"
@@ -29,8 +55,6 @@ def start():
 def level1():
     """Level 1 -- SQL Injection -- ' OR 'a'='a';--"""
 
-    session.clear()
-
     if request.method == "POST":
 
         username = request.form.get("username")
@@ -43,8 +67,12 @@ def level1():
 
         if len(user) != 1:
             abort(403)
+        
+        # Remember which user has logged in
+        session["user_id"] = user[0]["name"]
 
-        flash(user[0]["name"])
+        flash("Welcome ")
+        flash(session["user_id"])
 
         return redirect("/level2")
 
@@ -53,10 +81,9 @@ def level1():
 
 
 @app.route('/level2', methods=['GET', 'POST'])
+@login_required
 def level2():
     """Level 2 -- Bruteforce"""
-
-    session.clear()
 
     if request.method == "POST":
 
@@ -71,10 +98,36 @@ def level2():
         if len(user) != 1 or not check_password_hash(user[0]["pass"], password):
             abort(403)
 
-        return redirect("/finish")
+        return redirect("/level3")
 
     else:
+        
         return render_template("level2.html")
+
+
+@app.route("/level3")
+#@login_required
+def level3():
+    """Level 3"""
+    p = get_location()
+    lat = p["lat"]
+    lon = p["lon"]
+
+    # Embed a map as an iframe on a page.
+    m = folium.Map([lat, lon], zoom_start=12)
+
+    folium.Marker(
+        location=[lat, lon],
+        tooltip="You are here!",
+        popup="Yuo",
+        icon=folium.Icon(icon="cloud"),
+    ).add_to(m)
+
+    # Set the iframe width and height
+    m.get_root().width = "50%"
+    iframe = m.get_root()._repr_html_()
+    
+    return render_template("level3.html", iframe=iframe)
     
 
 @app.route("/about")
@@ -84,6 +137,7 @@ def about():
 
 
 @app.route("/finish")
+@login_required
 def finish():
     """Finish game"""
     return render_template("finish.html")
--- helpers.py
+++ helpers.py
@@ -3,6 +3,11 @@ from string import ascii_letters, digits, punctuation
 from itertools import product
 import requests
 
+# Level3 required
+from flask import redirect, render_template, session
+from functools import wraps
+
+
 
 # Function that sends a request to a login page
 def send_request(url, username, password):
@@ -24,3 +29,28 @@ def bruteforce(url, username):
         if response.text.find("Forbidden") == -1:
             print(f"The correct password is: {passcode}")
             break
+
+def login_required(f):
+    """
+    Decorate routes to require level2.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+# Function that provides geolocalization
+def get_ip():
+    response = requests.get('https://api.ipify.org?format=json').json()
+    return response["ip"]
+
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'http://ip-api.com/json/{ip_address}').json()
+    return response
